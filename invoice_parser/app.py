443dc5add6abe8e88fa9cf29055b08c2f2b05c3a
Hamza Farhan <thehamza96@gmail.com>
2023-08-16 01:07:42 +0500

    qry

--- invoice_parser/app.py
+++ invoice_parser/app.py
@@ -3,7 +3,7 @@ from invoice_parser.utils import *
 from invoice_parser.core import *
 from fastapi.responses import JSONResponse
 from langchain_ray.remote_utils import handle_input_path
-from fastapi import FastAPI, File, UploadFile, Form, HTTPException
+from fastapi import FastAPI, File, UploadFile, Form, HTTPException, Query
 
 app = FastAPI(
     title="Wilson Tools Parser",
@@ -12,21 +12,25 @@ app = FastAPI(
 
 llm_chain = qa_llm_chain()
 
+
 def endpoint(path, get_parts=True):
     msg.info(f"Path: {path}", spaced=True)
     path, bucket_path = handle_input_path(path)
     path = Path(path) / Path(bucket_path).name
     msg.info(f"Received path: {bucket_path}, Local Path: {path}", spaced=True)
     res = pdf_to_info_order_json(path, llm_chain, get_parts=get_parts, max_tries=1)
+
     res_json = {"info": res["info"]["json"], "order": res["order"]["json"]}
-    for k,v in res_json.items():
+    for k, v in res_json.items():
         if len(v) == 0:
-            raise HTTPException(status_code=500, detail=f"Unable to extract {k} information. Please try again.")
+            raise HTTPException(
+                status_code=500, detail=f"Unable to extract {k} information. Please try again."
+            )
     return JSONResponse(content=res_json)
 
 
 @app.post("/parse_po")
-def po_action(path: str = Field(title="Path to PDF file.")):
+def po_action(path: str = Query(..., title="Path to PDF file.")):
     return endpoint(path, get_parts=True)
     # msg.info(f"Path: {path}", spaced=True)
     # path, bucket_path = handle_input_path(path)
@@ -41,7 +45,7 @@ def po_action(path: str = Field(title="Path to PDF file.")):
 
 
 @app.post("/parse_ap")
-def ap_action(path: str = Field(title="Path to PDF file.")):
+def ap_action(path: str = Query(..., title="Path to PDF file.")):
     return endpoint(path, get_parts=False)
     # msg.info(f"Path: {path}", spaced=True)
     # path, bucket_path = handle_input_path(path)

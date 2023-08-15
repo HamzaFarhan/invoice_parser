cb195b6c447626374662348d9a9614e6dcafd83f
Hamza Farhan <thehamza96@gmail.com>
2023-08-15 07:48:20 +0500

    app

--- invoice_parser/app.py
+++ invoice_parser/app.py
@@ -1,9 +1,9 @@
 from invoice_parser.imports import *
 from invoice_parser.utils import *
 from invoice_parser.core import *
-
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI, File, UploadFile, Form
+from langchain_ray.remote_utils import handle_input_path
 
 app = FastAPI(
     title="Wilson Tools Parser",
@@ -12,9 +12,12 @@ app = FastAPI(
 
 llm_chain = qa_llm_chain()
 
+
 @app.post("/parse_po")
 def po_action(path: str):
     msg.info(f"Path: {path}", spaced=True)
+    path, bucket_path = handle_input_path(path)
+    msg.info(f"Received path: {bucket_path}, Local Path: {path}", spaced=True)
     res = pdf_to_info_order_json(path, llm_chain, get_parts=True)
     res_json = {"info": res["info"]["json"], "order": res["order"]["json"]}
     return JSONResponse(content=res_json)
@@ -23,6 +26,8 @@ def po_action(path: str):
 @app.post("/parse_ap")
 def ap_action(path: str):
     msg.info(f"Path: {path}", spaced=True)
+    path, bucket_path = handle_input_path(path)
+    msg.info(f"Received path: {bucket_path}, Local Path: {path}", spaced=True)
     res = pdf_to_info_order_json(path, llm_chain, get_parts=False)
     res_json = {"info": res["info"]["json"], "order": res["order"]["json"]}
     return JSONResponse(content=res_json)
